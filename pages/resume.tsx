a1bce08225f1b4a7fea56945c2659211a7d43d50
jxianc <jingxianchai01@gmail.com>
2022-05-10 01:10:50 -0500

    chore: add resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -6,7 +6,13 @@ interface ResumeProps {}
 const Resume: NextPage<ResumeProps> = ({}) => {
   return (
     <Container>
-      <div>resume</div>
+      <h1 className="text-lg font-bold mb-8">Resume</h1>
+      <iframe
+        src="https://www.keepandshare.com/doc27/112709/jingxian-chai-resume-pdf-99k?da=y"
+        width="100%"
+        height="700"
+        allow="autoplay"
+      ></iframe>
     </Container>
   )
 }
